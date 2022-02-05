010d62cabdf505ccc1bc69264f0040d264750b97
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2022-02-05 15:26:23 +0530

    ex7

---
+++ WEEK7/k_way.py
@@ -0,0 +1,18 @@
+from heapq import merge
+ 
+# function for meging k arrays
+def mergeK(arr, k):
+    l = []
+    for i in range(k):
+        l = list(merge(l, arr[i]))
+    return l
+ 
+
+sortedList =[[2, 6,],
+    [ 10,11],
+    [23, 34, 90],[7,21,38,93]]
+k = 4
+ 
+l = mergeK(sortedList, k)
+ 
+print(l)
